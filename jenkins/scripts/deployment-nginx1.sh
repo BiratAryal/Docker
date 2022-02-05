36ad84a545f6bcea492d7b474e2642746458a626
BiratAryal <birataryal1998@gmail.com>
2022-02-05 20:45:49 +0545

    Updated on on 2022/02/05 at 08:45:47 PM

--- jenkins/scripts/deployment-nginx1.sh
+++ jenkins/scripts/deployment-nginx1.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+PATH_HTML='/home/turboscript/Containers/Primary/Nginx/html'
+PATH_COMPOSE='/home/turboscript/Containers/Primary/Portainer/docker-compose.yml'
 docker container stop nginx nginx2
 docker container rm nginx nginx2
-rm -f /home/turboscript/Containers/Primary/Nginx/html/*
-echo -e "this is deployment 1 \n success" > index.html
-docker-compose -f /home/turboscript/Containers/Primary/Portainer/docker-compose.yml build
-docker-compose -f /home/turboscript/Containers/Primary/Portainer/docker-compose.yml up -d
-docker container ls
+rm -f $PATH_HTML/*
+echo -e "this is deployment 1 \n success" > $PATH_HTML/index.html
+docker-compose -f $PATH_COMPOSE build
+docker-compose -f PATH_COMPOSE up -d
